0f365060fd4019448d360c8c7df12629cf7bb60c
Nils <WagnerNils>
2021-06-18 13:36:27 +0200

    clean up splicemap

--- splicemap/splice_map.py
+++ splicemap/splice_map.py
@@ -7,7 +7,6 @@ class SpliceMap:
     def __init__(self, df, name):
         self.df = df
         self.name = name
-        # self.method = self._infer_method(self.ref_tables)
         self.method = self._infer_method(self.df)
 
     @classmethod
@@ -20,19 +19,6 @@ class SpliceMap:
             # TODO: split by first :
             name = line.split(':')[1].strip()
             return cls(pd.read_csv(f, **kwargs), name)
-
-    # def save_combined_ref_tables(self, save_path):
-    #     self.df.reset_index().to_csv(save_path, index=False)
-
-    # @staticmethod
-    # def _infer_method(ref_tables):
-    #     method = list()
-    #     for df in ref_tables:
-    #         if 'k' in df.columns and 'n' in df.columns:
-    #             method.append('kn')
-    #         elif 'alpha' in df.columns and 'beta' in df.columns:
-    #             method.append('bb')
-    #     return method
 
     @staticmethod
     def _infer_method(df):
@@ -66,6 +52,3 @@ class SpliceMap:
     @staticmethod
     def fetch(tissue_name):
         raise NotImplementedError()
-
-
-# df_gtf_junc = gr_gtf.features.introns(by='transcript').df
