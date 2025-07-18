6a64fa5d757eac6bd8bd1c3be6c440231e6eabb4
Aleksandr Neverov <l_neverov@ouga21.cmm.in.tum.de>
2025-07-18 21:35:31 +0200

    Add devSpliceMap download

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gagneurlab/splicemap',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False
 )
